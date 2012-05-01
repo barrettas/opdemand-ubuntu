c9a761a59cf9413c3f9dae68dab9bcde6e2a3d0b
gabrtv <gabriel@opdemand.com>
2012-05-01 10:32:59 -0600

    add deploy_example.sh for non-puppet stacks

---
+++ scripts/deploy_example.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Example deploy script for OpDemand-powered Ubuntu servers
+# NOTE: this script must be idemptoent!
+
+# source orchestration inputs as environment vars
+. /var/cache/opdemand/inputs.sh
+
+# set locale for calls that require encoding
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+# prevent dpkg prompting
+export DEBIAN_FRONTEND=noninteractive
+
+# install fail2ban
+apt-get install -yq fail2ban
